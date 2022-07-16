a646d5dd7fb45214a305afc412dcd35d98c5bf57
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-07-16 23:58:48 +0300

    issue 11: Sentry.ErrorBoundary added

--- index.tsx
+++ index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LandingPage } from './src/pages/landing/landing.page';
 import { Page404 } from './src/pages/404/404.page';
 import { Layout } from './src/pages/layout/layout';
 import { SignInPage } from './src/pages/sign-in/sign-in.page';
 import { SignUpPage } from './src/pages/sign-up/sign-up.page';
-import { GlobalErrorBoundary } from './src/components/global-error-boundary/global-error-boundary';
+import { UnexpectedErrorPage } from './src/pages/unexpected-error/unexpected-error';
+
+Sentry.init({
+  dsn: 'https://291227dabf594d61b4b8435635794c05@o1321771.ingest.sentry.io/6578460',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GlobalErrorBoundary>
+  <Sentry.ErrorBoundary fallback={<UnexpectedErrorPage />}>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
@@ -22,5 +34,5 @@ root.render(
         </Route>
       </Routes>
     </BrowserRouter>
-  </GlobalErrorBoundary>,
+  </Sentry.ErrorBoundary>,
 );
