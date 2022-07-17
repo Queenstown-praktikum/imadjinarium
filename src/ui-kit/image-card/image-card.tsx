7ac4477a14dd3c2eca0b2c0fdc846add7021bb96
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-07-17 12:47:01 +0300

    issue 13: support of css modules in storybook

--- src/ui-kit/image-card/image-card.tsx
+++ src/ui-kit/image-card/image-card.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
+import styles from './image-card.scss';
 
 type Props = {
   caption: string;
 };
 
-export const ImageCard: FC<Props> = ({ caption = '' }) => <div>{caption}</div>;
+export const ImageCard: FC<Props> = ({ caption = '' }) => <div className={styles['image-card']}>{caption}</div>;
