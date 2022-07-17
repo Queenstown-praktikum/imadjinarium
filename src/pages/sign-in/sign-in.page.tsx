08cc5294df090f4226f9fc32af8ca7ddcfc147e4
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-07-17 14:47:05 +0300

    issue 13: player-selection page

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,10 +1,8 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
-import { ImageCard } from '../../ui-kit/image-card/image-card';
 
 export const SignInPage: FC = () => (
   <div className={cn('fullscreen', 'centered')}>
     <h2>SignInPage</h2>
-    <ImageCard imageUrl='https://papik.pro/uploads/posts/2021-09/1631229101_2-papik-pro-p-imadzhinarium-illyustratsii-2.jpg' />
   </div>
 );
