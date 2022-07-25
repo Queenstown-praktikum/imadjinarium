0add1ed9e9faaace78550052f689a9f5eee62ab4
dev <drozdovdn>
2022-07-25 22:20:54 +0300

    issues 27: add file server

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const PORT = 5000;
+
+const port = PORT;
+
+const app = express();
+
+app.use(express.static('dist'));
+
+// @ts-ignore
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на  http://localhost:${port}`);
+});
