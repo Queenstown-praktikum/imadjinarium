826e486a2b797e0fcea7d19fdd0915cd36c87054
Eresko Egor <@ereskoea>
2022-08-05 08:54:00 +0300

    fix lost component

--- src/ui-kit/index.ts
+++ src/ui-kit/index.ts
@@ -1,4 +1,4 @@
 export { default as TextField } from './textField/TextField';
 export { default as FormWrapper } from './formWrapper/FormWrapper';
-export { default as Button } from './button/Button';
+export { default as Button } from './button/Btn';
 export { default as ImageCard } from './image-card/ImageCard';
