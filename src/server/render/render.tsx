cb627a83215ec8e0c654ccb390b03572eb58c4db
drozdov <drozdov.dn29@gmail.com>
2022-08-25 21:15:23 +0300

    add style ssr

--- src/server/render/render.tsx
+++ src/server/render/render.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { Request, Response } from 'express';
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
 import { store } from '../../redux/store';
@@ -11,7 +11,7 @@ export function render(req: Request, res: Response) {
   const { devMiddleware } = res.locals.webpack;
   const jsonWebpackStats = devMiddleware.stats.toJson();
   const { assetsByChunkName } = jsonWebpackStats;
-  // const css = assetsByChunkName.main[0];
+  const css = assetsByChunkName.main[0];
   const script = assetsByChunkName.main[1];
 
   delete require.cache[require.resolve('../../../dist/ssr.bundle.js')];
@@ -27,15 +27,31 @@ export function render(req: Request, res: Response) {
     </StaticRouter>,
   );
 
-  const html = fs.readFileSync(path.join(__dirname, '../../../public/index.html'), {
-    encoding: 'utf-8',
-  });
+  // const html = fs.readFileSync(path.join(__dirname, '../../../public/index.html'), {
+  //   encoding: 'utf-8',
+  // });
 
-  return res.status(200).send(
-    html.replace(
-      '<div id="root"></div>',
-      `<div id="root">${reactHtml}</div>
-                   <script src='${script}'></script>`,
-    ),
-  );
+  const html = `
+  <!doctype html>
+  <html lang="ru">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link rel="manifest" href='manifest.json'>
+      <link rel="stylesheet" href="${css}">
+      <title>Imadjinarium</title>
+  </head>
+  <body>
+      <div id="root">${reactHtml}</div>
+      <script src="${script}"></script>
+  </body>
+  </html>`;
+
+  return res.status(200).send(html);
 }
