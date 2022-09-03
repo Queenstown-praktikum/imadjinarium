051e62d8512a34cfa958641f5b559eb363a32192
drozdov <drozdov.dn29@gmail.com>
2022-09-03 17:30:06 +0300

    fix style status panel

--- src/features/status-panel/status-panel.tsx
+++ src/features/status-panel/status-panel.tsx
@@ -1,18 +1,27 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
+import { Button } from 'ui-kit';
 import styles from './status-panel.scss';
 import { ItemStatus } from './sub-components/item-status';
 import { mockData } from './mock';
 
-export const StatusPanel: FC = () => (
-    <div className={cn(styles.sp)}>
-      <h3 className={cn(styles.sp__title)}>Статусы игроков</h3>
-      <div className={cn(styles.sp__body)}>
-        {Object.entries(mockData).map(([key, item]) => (
-            <div className={cn(styles.sp__item)}>
-              <ItemStatus key={key} data={item} />
-            </div>
-          ))}
-      </div>
+type StatusPanelProps = {
+  title: string;
+  button?: boolean;
+};
+
+export const StatusPanel: FC<StatusPanelProps> = ({ title, button }) => (
+  <div className={cn(styles.sp)}>
+    <div className={styles.sp__header}>
+      <h3 className={cn(styles.sp__title)}>{title}</h3>
+      {button && <Button label="Начать" />}
     </div>
-  );
+    <div className={cn(styles.sp__body)}>
+      {Object.entries(mockData).map(([key, item]) => (
+        <div>
+          <ItemStatus key={key} data={item} />
+        </div>
+      ))}
+    </div>
+  </div>
+);
--- src/pages/player-selection/player-selection.page.tsx
+++ src/pages/player-selection/player-selection.page.tsx
@@ -1,12 +1,15 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import cn from 'classnames';
 import { Button, ImageCard } from 'ui-kit';
 import styles from './player-selection.scss';
 import { useHandOutCards } from '../../hooks/useHandOutCards';
 import { Input } from '../../ui-kit/input/input';
+import { Modal } from '../../ui-kit/modal/modal';
+import { StatusPanel } from '../../features/status-panel/status-panel';
 
 export const PlayerSelectionPage: FC = () => {
   const { usersCards, user, value, setValue } = useHandOutCards();
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const cartList = useCallback(
     (id: number) => (
@@ -19,18 +22,29 @@ export const PlayerSelectionPage: FC = () => {
   const onChange = (e: React.FocusEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
+  const handleClickButton = () => {
+    setShowModal(true);
+  };
+
   return (
-    <div className={cn(styles['player-selection-page'], 'fullscreen')}>
-      <span className={styles['player-selection-page__title']}>Вы ведущий</span>
-      <p className={styles['player-selection-page__desc']}>Выберете карту и загадайте на неё ассоциацию</p>
-      <ul className={styles['player-selection-page__cards']}>{usersCards?.[user?.id]?.map(cartList)}</ul>
-      <div className={styles['player-selection-page__input']}>
-        <span className={styles['player-selection-page__label']}>Ассоциация</span>
-        <div className={styles['player-selection-page__wrapper-input']}>
-          <Input value={value} onChange={onChange} />
-          <Button label="Готово" disabled={value === ''} />
+    <>
+      <div className={cn(styles['player-selection-page'], 'fullscreen')}>
+        <span className={styles['player-selection-page__title']}>Вы ведущий</span>
+        <p className={styles['player-selection-page__desc']}>Выберете карту и загадайте на неё ассоциацию</p>
+        <ul className={styles['player-selection-page__cards']}>{usersCards?.[user?.id]?.map(cartList)}</ul>
+        <div className={styles['player-selection-page__input']}>
+          <span className={styles['player-selection-page__label']}>Ассоциация</span>
+          <div className={styles['player-selection-page__wrapper-input']}>
+            <Input value={value} onChange={onChange} />
+            <Button label='Готово' disabled={value === ''} onClick={handleClickButton} />
+          </div>
         </div>
       </div>
-    </div>
+      {showModal && (
+        <Modal>
+          <StatusPanel title="Ждем остальных игроков" />
+        </Modal>
+      )}
+    </>
   );
 };
